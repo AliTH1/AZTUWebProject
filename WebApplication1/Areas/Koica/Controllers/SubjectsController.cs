3e60d25ad5112efdd2ceb9522c875f31be86f69a
Ali Taghizada <alitaqizade2@gmail.com>
2023-06-20 14:10:59 +0400

    subjects added

---
+++ WebApplication1/Areas/Koica/Controllers/SubjectsController.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Areas.Koica.Controllers
+{
+    [Area("Koica")]
+    public class SubjectsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SubjectsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Notification(int id)
+        {
+            return View(await _context.Subjects.FirstOrDefaultAsync(c => c.Id == id));
+        }
+
+        public async Task<IActionResult> Forum(int id)
+        {
+            return View(await _context.Subjects.FirstOrDefaultAsync(c => c.Id == id));
+
+        }
+
+        public async Task<IActionResult> DidacticMaterials(int id)
+        {
+            return View(await _context.Subjects.FirstOrDefaultAsync(c => c.Id == id));
+        }
+
+        public async Task<IActionResult> Evaluation(int id)
+        {
+            return View(await _context.Subjects.FirstOrDefaultAsync(c => c.Id == id));
+        }
+    }
+}
